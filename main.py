29eddb6b76b72d62facc01300811a442fa5fbcae
Bábinszki Botond <babinszkibotond@gmail.com>
2025-02-01 18:26:57 +0100

    Finished Data Janitoring

--- main.py
+++ main.py
@@ -1,10 +1,51 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from imblearn.over_sampling import RandomOverSampler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report
+
+def scale_dataset(dataframe, oversample=False):
+  X = dataframe[dataframe.columns[:-1]].values
+  y = dataframe[dataframe.columns[-1]].values
+
+  scaler = StandardScaler()
+  X = scaler.fit_transform(X)
+
+  if oversample:
+    ros = RandomOverSampler()
+    X, y = ros.fit_resample(X, y)
+
+  data = np.hstack((X, np.reshape(y, (-1, 1))))
+
+  return data, X, y
 
 labels = ["fLength", "fWidth", "fSize", "fConc", "fConc1", "fAsym", "fM3Long", "fM3Trans", "fAlpha", "fDist", "class"]
 dataset = pd.read_csv("Data/magic04.data", names = labels)
+# print(dataset.isnull().sum()) check if the dataset has null values
 # print(dataset.head())
 # print(dataset["class"].unique())
 dataset["class"] = np.where(dataset["class"] == "g", 1, 0)
-print(dataset.head())
+#print(dataset.head())
+
+for label in labels:
+  plt.hist(dataset[dataset["class"]==1][label], color='blue', label='gamma', alpha=0.7, density=True)
+  plt.hist(dataset[dataset["class"]==0][label], color='red', label='hadron', alpha=0.7, density=True)
+  plt.title(label)
+  plt.ylabel("Probability")
+  plt.xlabel(label)
+  plt.legend()
+  # plt.show()
+
+# creating the sets
+train, valid, test = np.split(dataset.sample(frac=1), [int(0.6*len(dataset)), int(0.8*len(dataset))])
+train, X_train, y_train = scale_dataset(train, oversample=True)
+valid, X_valid, y_valid = scale_dataset(valid, oversample=False)
+test, X_test, y_test = scale_dataset(test, oversample=False)
+
+# using KNN model to look at the data
+knn_model = KNeighborsClassifier(n_neighbors=9)
+knn_model.fit(X_train, y_train)
+y_pred = knn_model.predict(X_test)
+print(classification_report(y_test, y_pred))
